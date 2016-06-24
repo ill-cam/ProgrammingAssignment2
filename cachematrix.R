b86f276f7746efcc944b09a91c3ec715c8886647
ill.cam <ill.cam.w@gmail.com>
2016-06-24 19:48:06 -0400

    Add existing file

--- cachematrix.R
+++ cachematrix.R
@@ -7,15 +7,19 @@
 ## matrix in the argument as well as the value of the matrix's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-  v <- NULL
-  set <- function(y) {
-    x <<- y
-    v <<- NULL
+  v <- NULL ## Creates a local variable to ultimately store the inverse matrix
+  set <- function(y) { ## Creates a function to cache the value of the matrix 
+## in the argument
+    x <<- y ## Special operator makes value of x available outside this function
+    v <<- NULL ## Special operator makes value of v available outside this function
   }
-  get <- function() x
-  setinv <- function(inv) v <<- inv
-  getinv <- function() v
-  list(set = set, get = get, setinv = setinv, getinv = getinv)
+  get <- function() x ## Creates a function to return the cached value of the matrix
+  setinv <- function(inv) v <<- inv ## Creates function that updates the cached
+## value of the inverse matrix
+  getinv <- function() v ## Creates function to return the cached value of the
+## inverse matrix
+  list(set = set, get = get, setinv = setinv, getinv = getinv) ## Creates a list of
+##functions to access from other functions in global environment
 }
 
 
@@ -23,13 +27,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## prints its value or just prints the inverse's value if already calculated
 
 cacheSolve <- function(x, ...) {
-  v <- x$getinv()
+  v <- x$getinv() ## Assigns cached inverse matrix value (may be null) to v
   if(!is.null(m)) {
     message("getting cached data")
-    return(v)
-  }
-  data <- x$get()
-  v <- solve(data)
-  x$setinv(v)
-  v
+    return(v) ## If there is a cached inverse matrix, returns the value and the
+##function is complete
+  } ## The rest of this code determines what happens if an inverse matrix value
+##has not been cached
+  data <- x$get() ## Accesses cached matrix value and assigns to new matrix ("data")
+  v <- solve(data) ## Calculates the inverse matrix for "data"
+  x$setinv(v) ## Caches the inverse matrix value
+  v ## Prints the inverse matrix value
 }
